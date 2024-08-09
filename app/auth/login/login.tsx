3e2edbdd2385e0637438f622dead54540b7f8fad
MyztKnt <kenta.miyazato@fox-hound.jp>
2024-08-09 11:18:35 +0900

    既存のloginページを改修
    一旦、さらで作るためにローカルブランチのみ作成

--- app/auth/login/login.tsx
+++ app/auth/login/login.tsx
@@ -2,9 +2,11 @@
 
 import { getFormProps, getInputProps, useForm } from '@conform-to/react';
 import { parseWithZod } from '@conform-to/zod';
-import { LockOutlined } from '@mui/icons-material';
+// import { LockOutlined } from '@mui/icons-material';
 import { Avatar, Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
-import NextLink from 'next/link';
+// import { url } from 'inspector';
+// import NextLink from 'next/link';
+// import { normalize } from 'path';
 import { useFormState } from 'react-dom';
 
 import { loginAction } from 'lib/actions/auth';
@@ -20,14 +22,10 @@ export default function Login({ onSubmit }: { onSubmit: typeof loginAction }) {
     shouldValidate: 'onBlur',
   });
   return (
-    <Container component="main" maxWidth="xs">
-      <Box className="mt-16 flex flex-col items-center">
-        <Avatar className="m-2" sx={{ bgcolor: 'secondary.main' }}>
-          <LockOutlined />
-        </Avatar>
-
-        <Typography component="h1" variant="h5">
-          Sign in
+    <div className="h-full w-full bg-red-600">
+      <Box className="mt-16 flex w-1/3 flex-col items-center bg-white">
+        <Typography component="h1" variant="h5" sx={{ color: '#002E6E', fontWeight: 'bold' }}>
+          業務管理システム
         </Typography>
         <Box component="form" action={action} {...getFormProps(form)} className="mt-2">
           <TextField
@@ -35,6 +33,8 @@ export default function Login({ onSubmit }: { onSubmit: typeof loginAction }) {
             required
             fullWidth
             label="メールアドレス"
+            InputLabelProps={{ shrink: true }}
+            placeholder="example@example.com"
             autoComplete="email"
             autoFocus
             {...getInputProps(fields.email, { type: 'email' })}
@@ -47,30 +47,30 @@ export default function Login({ onSubmit }: { onSubmit: typeof loginAction }) {
             required
             fullWidth
             label="パスワード"
+            InputLabelProps={{ shrink: true }}
+            placeholder="********"
             autoComplete="current-password"
             {...getInputProps(fields.password, { type: 'password' })}
             key={fields.password.key}
             error={!!fields.password.errors}
             helperText={fields.password.errors}
+            style={{ marginBottom: '0px' }}
           />
+          <Typography variant="overline" color="textSecondary" style={{ marginLeft: '16px' }}>
+            パスワードを忘れた方は管理者にご連絡ください
+          </Typography>
           {form.errors && <div className="text-center text-red-600">{form.errors}</div>}
-          <Button type="submit" fullWidth variant="contained" className="mb-4 mt-6">
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            className="mb-4 mt-6"
+            style={{ backgroundColor: '#3F51B5' }}
+          >
             ログイン
           </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link component={NextLink} href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link component={NextLink} href="register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
         </Box>
       </Box>
-    </Container>
+    </div>
   );
 }
